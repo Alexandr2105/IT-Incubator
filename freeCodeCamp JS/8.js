2434f9f0982842db7d2fd4a60f13c44a7841f6e3
Alexandr2105 <5030553@gmail.com>
2022-08-27 20:17:59 +0300

    new comment

---
+++ freeCodeCamp JS/8.js
@@ -0,0 +1,8 @@
+// Понимание чувствительности к регистру в переменных
+var studLycapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studLycapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
