5b9418575d530c6b187d73a2de291e1e22b37b68
Alexandr2105 <5030553@gmail.com>
2022-08-27 13:20:15 +0300

    new comment

---
+++ codewar JS/6.js
@@ -0,0 +1,11 @@
+function amIWilson(p) {
+    switch(p){
+        case 5:
+        case 13:
+        case 563:
+        return true;
+        break;
+        default:
+        return false;
+    }
+  }
