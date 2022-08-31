be5865405992c50b704a988009455d297cd68a43
Alexandr2105 <5030553@gmail.com>
2022-08-31 20:13:34 +0300

    new comment

---
+++ freeCodeCamp JS/59.js
@@ -0,0 +1,7 @@
+// Используйте условную логику с операторами If
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+}
