dfb4c99beaf59e5fadd664bb93064a34d3e62833
Alexandr2105 <5030553@gmail.com>
2022-09-04 13:22:29 +0300

    new comment

---
+++ freeCodeCamp JS/84.js
@@ -0,0 +1,9 @@
+//Доступ к свойствам объекта с помощью записи через точку
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
