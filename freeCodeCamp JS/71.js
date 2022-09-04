3b0d0a383e04b0b910dd51e92fef5b05cac083a0
Alexandr2105 <5030553@gmail.com>
2022-09-04 11:45:45 +0300

    new comment

---
+++ freeCodeCamp JS/71.js
@@ -0,0 +1,12 @@
+// Знакомство с операторами Else
+function testElse(val) {
+    let result = "";
+    if (val > 5) {
+        result = "Bigger than 5";
+    } else {
+        result = "5 or Smaller";
+    }
+    return result;
+}
+
+testElse(4);
