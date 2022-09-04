b0ec547b632b3a9afa190ab456d4f2bf2053df8e
Alexandr2105 <5030553@gmail.com>
2022-09-04 11:24:14 +0300

    new comment

---
+++ freeCodeCamp JS/66.js
@@ -0,0 +1,14 @@
+// Сравнение с оператором больше или равно
+function testGreaterOrEqual(val) {
+    if (val>=20) {
+        return "20 or Over";
+    }
+
+    if (val>=10) {
+        return "10 or Over";
+    }
+
+    return "Less than 10";
+}
+
+testGreaterOrEqual(10);
