bb7625262a84c6963a75cd252200d9dfd8472815
Alexandr2105 <5030553@gmail.com>
2022-09-04 13:55:29 +0300

    new comment

---
+++ freeCodeCamp JS/86.js
@@ -0,0 +1,9 @@
+//Доступ к свойствам объекта с помощью переменных
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+const playerNumber = 16;
+const player = testObj[playerNumber];
