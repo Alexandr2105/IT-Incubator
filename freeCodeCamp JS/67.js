3bf09a67f7f768b904935aa2207e94366be2bc0d
Alexandr2105 <5030553@gmail.com>
2022-09-04 11:25:46 +0300

    new comment

---
+++ freeCodeCamp JS/67.js
@@ -0,0 +1,14 @@
+// Сравнение с оператором меньше, чем
+function testLessThan(val) {
+    if (val<25) {
+        return "Under 25";
+    }
+
+    if (val<55) {
+        return "Under 55";
+    }
+
+    return "55 or Over";
+}
+
+testLessThan(10);
