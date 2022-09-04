95959312c9b7fcaf19b8ca8bc200a7cc9ab300b0
Alexandr2105 <5030553@gmail.com>
2022-09-04 21:15:24 +0300

    new comment

---
+++ freeCodeCamp JS/92.js
@@ -0,0 +1,24 @@
+// Управление сложными объектами
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "qwer",
+        "title": "asdf",
+        "release_year": 234,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ]
+    }
+];
