d6243253d4796f902d36025b2e9f415c3f1b31c6
Alexandr2105 <5030553@gmail.com>
2022-09-08 13:02:25 +0300

    new comment

---
+++ freeCodeCamp JS/ES6/21.js
@@ -0,0 +1,10 @@
+//Используйте экспорт, чтобы поделиться блоком кода
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
+
+export {uppercaseString, lowercaseString};
