d8c0b31b7e50bd0a5cf6bf8c15094aa53c2b456f
Alexandr2105 <5030553@gmail.com>
2022-09-08 13:06:16 +0300

    new comment

---
+++ freeCodeCamp JS/ES6/22.js
@@ -0,0 +1,5 @@
+//Повторное использование кода JavaScript с помощью импорта
+import {uppercaseString, lowercaseString} from './string_functions.js'
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
